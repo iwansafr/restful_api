df8b5d89135c58dce2ef9ab8482d7c341e2ca43f
iwansafr <iwansafr@gmail.com>
2019-12-25 12:05:32 +0700

    basic preparation for restful

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        return 'Its works';
+    }
+
+    public function signin(Request $request)
+    {
+        return 'Its works';
+    }
+}
